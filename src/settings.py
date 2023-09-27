8e1e5833803bb4fd2291eec4fb1fb44d3023ca92
erastov-alex <erastov.alex@gmail.com>
2023-09-27 17:00:06 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Corridor Floor Segmentation"
 PROJECT_NAME_FULL: str = "Corridor Floor Segmentation"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
