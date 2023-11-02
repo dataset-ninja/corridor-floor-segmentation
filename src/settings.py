c2a01cd3caa4e4a213a1f6f76848571926deba1d
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-03 00:01:31 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -59,6 +59,7 @@ REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
 AUTHORS: Optional[List[str]] = ["Raptor"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["https://www.kaggle.com/deepakmedam"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
@@ -112,6 +113,7 @@ def get_settings():
     settings["repository"] = REPOSITORY
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
+    settings["authors_contacts"] = AUTHORS_CONTACTS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
